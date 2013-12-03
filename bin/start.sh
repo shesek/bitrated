889f63db366aa95f0983986d16377cfeb0b35fec
Nadav Ivgi <git@shesek.info>
2013-12-03 07:15:16 +0200

    Restart nodejs server on crashes

--- bin/start.sh
+++ bin/start.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 [ -f .env ]  && source .env
 
-./node_modules/.bin/coffee ./server/app.coffee "$@"
+# Kill child process on exit
+trap 'kill $(jobs -p)' EXIT
+
+# Re-run server on crashes
+while :
+do
+  ./node_modules/.bin/coffee ./server/app.coffee "$@"
+  echo "Server crashed with status $?"
+  sleep 1
+done
 
